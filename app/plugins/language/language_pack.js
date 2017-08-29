7970e3295c20a8fef768f066ad23dd45ed1fc32e
mygongsun <honorxp@126.com>
2017-08-29 12:50:24 +0800

    Developer

--- app/plugins/language/language_pack.js
+++ app/plugins/language/language_pack.js
@@ -1,25 +1,9 @@
 // 语言包文件
 
 var translationsEN = {
-    TITLE: 'Inter Learn Chinese',
-    LOGIN_A: 'Teaching management platform',
-    INPUT_EMAIL: 'Please enter your login mailbox',
-    INPUT_PWD: 'Please enter your login mailbox',
-	FORGET_PWD: 'Forget password?',
-	NOT_ACCOUNT: 'Have no ACCOUNT?',
-    LOGIN_BTN: 'Login',
-    INPUT_ADDRESS: 'Enter email address',
-    NEXT: 'Next step'
+
 };
 
 var translationsCN= {
-    TITLE: '互动学汉语',
-    LOGIN_A: '教&nbsp;&nbsp;学&nbsp;&nbsp;管&nbsp;&nbsp;理&nbsp;&nbsp;平&nbsp;&nbsp;台',
-    INPUT_EMAIL: '请输入登陆邮箱',
-    INPUT_PWD: '请输入登陆密码',
-    FORGET_PWD: '忘记密码?',
-    NOT_ACCOUNT: '还没有账号？',
-    LOGIN_BTN: '登录',
-    INPUT_ADDRESS: '请输入邮箱地址',
-    NEXT: '下一步'
+
 };
